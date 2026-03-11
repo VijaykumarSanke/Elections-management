810758d8516cd0b39b10929489612d004e192931
VijaykumarSanke <vijaykumar.sanke7@gmail.com>
2026-03-11 22:10:34 +0530

    Delete election.js

--- election.js
+++
@@ -1,9 +0,0 @@
-function nextCategory(currentCategory) {
-    const currentCategoryDiv = document.getElementById(`category${currentCategory}`);
-    const nextCategoryDiv = document.getElementById(`category${currentCategory + 1}`);
-
-    if (nextCategoryDiv) {
-        currentCategoryDiv.style.display = 'none';
-        nextCategoryDiv.style.display = 'block';
-    }
-}
